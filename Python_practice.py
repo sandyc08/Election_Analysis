1484f53785eac9d15586b97ab222f6556f57ed81
Sandy Cisneros <sandyc08@yahoo.com>
2022-06-14 23:01:32 -0700

    Adding new files

---
+++ Python_practice.py
@@ -0,0 +1,4 @@
+voting_data = {"Arapahoe": 369237,"Denver": 413229 ,"Jefferson": 390222}
+for county, voters in voting_data.items():
+    print(f"{county} county has {voters} registered voters.")
+    
